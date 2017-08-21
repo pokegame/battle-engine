7ef294d4409b8d833596f059db29b49d8942aae6
Federkun <federkun@gmail.com>
2017-08-21 20:52:31 +0200

    Add pokémon type definition

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+/**
+ * Types are properties for Pokémon and their moves. 
+ * A Pokémon may have either one or two types.
+ * A move has exactly one type.
+ */
+export type Type = 'normal'
+  | 'fire'
+  | 'fighting'
+  | 'water'
+  | 'flying'
+  | 'grass'
+  | 'poison'
+  | 'electric'
+  | 'ground'
+  | 'psychic'
+  | 'rock'
+  | 'ice'
+  | 'bug'
+  | 'dragon'
+  | 'ghost'
+  | 'dark'
+  | 'steel'
+  | 'fairy'
+;
