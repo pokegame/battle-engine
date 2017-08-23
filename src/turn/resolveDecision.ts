7ccb8d1dc93f1835cbcc8b53778daa243f1cb37a
Federkun <federkun@gmail.com>
2017-08-23 10:20:32 +0200

    cannot decide to attack with a fainted pokemon

--- src/turn/resolveDecision.ts
+++ src/turn/resolveDecision.ts
@@ -11,6 +11,11 @@ export function resolveDecision(battleState: types.BattleState, battleDecision: 
   if (battleDecision.decision === 'attack') {
     // The battler Pokémon's name of this actor.
     const attackerName = battleState.battlers[battleDecision.actor];
+    const attacker = battleState.pokemon[attackerName];
+
+    if (isFainted(attacker)) {
+      throw new DecisionError('Cannot attack with a fainted pokémon.');
+    }
 
     if (!(battleDecision.move in battleState.moves[attackerName])) {
       throw new DecisionError('This pokémon does not have this move.');
@@ -31,7 +36,7 @@ export function resolveDecision(battleState: types.BattleState, battleDecision: 
       // The attacks with the highest priority go first.
       // If two Pokémon both use an attack with the same priority,
       // then the speed stat of each Pokémon comes into play.
-      priorities: [8, move.dex.priority, decisionSpeed(battleState.pokemon[attackerName])]
+      priorities: [8, move.dex.priority, decisionSpeed(attacker)]
     };
   }
 
