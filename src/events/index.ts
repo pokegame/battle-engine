5d93634c00666bb907c46953a8441da73a087b1c
Federkun <federkun@gmail.com>
2017-08-27 21:47:30 +0200

    Add a basic implementation of switchCommand

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,3 +1,4 @@
 export { hitPointChanged } from './hitPointChanged';
 export { moveHit } from './moveHit';
 export { moveMissed } from './moveMissed';
+export { pokemonSwitched } from './pokemonSwitched';
